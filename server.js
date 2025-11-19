1245cd615b7ecb67ff0dd88ee3e281fbee589fbc
balu2813 <balupathakokila2121@gmail.com>
2025-11-19 10:52:57 +0530

    handled-auth-functionality

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import dotenv from "dotenv"
 import express from "express"
 import cors from "cors"
 import path from "path"
+import authRoutes from './routes/authRoutes.js'
+import dashboardRoutes from './routes/dashboardRoutes.js'
 import  connectDB  from "./config/db.js";
 dotenv.config();
 
@@ -22,6 +24,8 @@ app.use(express.json());
 
 connectDB();
 
+app.use('/api/auth',authRoutes);
+app.use('/api/dashboard',dashboardRoutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
